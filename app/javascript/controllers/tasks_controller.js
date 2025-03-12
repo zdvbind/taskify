7cbbc32e30bd90cc8ac7c9afd7e9bf7457ad478c
Dmitry Zaguta <zdvbind@gmail.com>
2025-03-12 14:30:34 +0500

    feat: add to-do list

---
+++ app/javascript/controllers/tasks_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tasks"
+export default class extends Controller {
+  connect() {
+    console.log(this.element)
+  }
+
+  toggle(e) {
+    const id = e.target.dataset.id
+    const csrfToken = document.querySelector("[name='csrf-token']").content
+
+    fetch(`/tasks/${id}/toggle`, {
+      method: 'POST',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify({ completed: e.target.checked })
+    })
+        .then(response => response.json())
+        .then(data => {
+          alert(data.message)
+        })
+  }
+}
